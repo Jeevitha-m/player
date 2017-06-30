49c60885162034cd86e50b633659b1408f3e4aea
Jeevitha-m <jeeveemega@gmail.com>
2017-06-30 10:08:29 +0530

    Create convertint.java

---
+++ convertint.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+class convertint
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        int a=Integer.parseInt(str);
+        System.out.println(a);
+    }
+ }   
